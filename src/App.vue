296767a41dc3ad35f35031948c5ca04434e4a621
Ferhat Avdic <ferhat.avdic@gmail.com>
2017-12-21 17:55:13 +0100

    full project

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+      <Navbar />
+    <div class="container">
+      <router-view/>
+      </div>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
 export default {
-  name: 'app'
+  name: 'app',
+  components:{
+    Navbar
+  }
 }
 
 </script>
 
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #1a5692;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDJiAa8sClfFR52_uJ_Eg_njQEWLvbv-wY",
+    authDomain: "basicvue-35efb.firebaseapp.com",
+    databaseURL: "https://basicvue-35efb.firebaseio.com",
+    projectId: "basicvue-35efb",
+    storageBucket: "basicvue-35efb.appspot.com",
+    messagingSenderId: "827322651030"
+}
